94622e2006c81d2b78fd0fa34feec9a3a1c5c38a
dwarren <dwarren@nmhschool.org>
2017-11-26 13:44:56 -0500

    setup https with letencrypt
    setup letsencrypt
    changed the cron job for scraping to once an hour at minute 59
    added helmet

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 var express = require("express");
 var https = require("https");
+var helmet = require('helmet');
+var fs = require("fs");
 var db = require('./db.js');
 var fb = require('./post.js');
 require('dotenv').config();
 var auth = require('./auth.js');
 var schedule = require('node-schedule');
 var app = express();
+app.use(helmet())
 
-schedule.scheduleJob('0 */10 * * * *', db.scrape);
+var options = {
+    cert: fs.readFileSync('./sslcert/fullchain.pem'),
+    key: fs.readFileSync('./sslcert/privkey.pem')
+};
+
+schedule.scheduleJob('0 59 * * * *', db.scrape);
 schedule.scheduleJob('0 0 21 * * *', fb.post);
 
 
@@ -31,6 +39,8 @@ app.get('/scrape', function(req, res) {
 
 app.get('/post', function(req, res) {
   fb.post();
+  res.send("done");
 })
 
 app.listen(8080,'localhost');
+https.createServer(options, app).listen(8443);
