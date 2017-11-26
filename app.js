24b38da4e9778a871dba8cb5ef0357669d30187f
dwarren <dwarren@nmhschool.org>
2017-11-26 16:15:01 -0500

    went back to where it was working

--- app.js
+++ app.js
@@ -1,34 +1,23 @@
 var express = require("express");
 var https = require("https");
-var http = require("http");
+var helmet = require('helmet');
 var fs = require("fs");
 var db = require('./db.js');
 var fb = require('./post.js');
 require('dotenv').config();
 var auth = require('./auth.js');
 var schedule = require('node-schedule');
+var app = express();
+app.use(helmet());
+app.use(secure);
+
+var options = {
+    cert: fs.readFileSync('./sslcert/fullchain.pem'),
+    key: fs.readFileSync('./sslcert/privkey.pem')
+};
 
 schedule.scheduleJob('0 59 * * * *', db.scrape);
 schedule.scheduleJob('0 0 21 * * *', fb.post);
-
-var options = {
-  cert: fs.readFileSync('./sslcert/fullchain.pem'),
-  key: fs.readFileSync('./sslcert/privkey.pem')
-};
-
-var app = express();
-
-app.use(function(req, res, next) {
-  if (!/https/.test(req.protocol)) {
-    res.redirect("https://" + req.headers.host + req.url);
-  } else {
-    return next();
-  }
-});
-
-var webServer = app.listen(443, function() {
-  console.log('Listening on port %d', webServer.address().port);
-});
 
 //the facebook oAuth stuff is in here
 auth.init(app);
@@ -52,3 +41,6 @@ app.get('/post', function(req, res) {
   fb.post();
   res.send("done");
 })
+
+app.listen(80,'localhost');
+https.createServer(options, app).listen(443);
